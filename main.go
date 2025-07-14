ece997ef0fc540faef6829169a7e1ac0bf24eab7
Axel <axel77g@hotmail.com>
2025-07-14 15:26:33 +0200

    try fix tcp not open

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"peer-to-peer/app/peer_comunication"
 	"peer-to-peer/app/shared"
 	"sync"
+	"time"
 )
 
 
@@ -13,11 +14,17 @@ func main() {
 	wg := sync.WaitGroup{}
 	wg.Add(6)
 
+	tcpServer := peer_comunication.NewTCPServer(shared.TCPPort)
+	go tcpServer.Listen(&handlers.TCPControllerTransportChannelHandler{}) //open tcp server listener with the tcp handler
+
+	//attend 2 seconde
+	time.Sleep(2 * time.Second)
+	
+
 	udpServer := peer_comunication.GetUDPServerListener()
 	go udpServer.Listen(&handlers.UDPDiscoveryTransportChannel{}) //open udp server listener with the discovery handler
 
-	tcpServer := peer_comunication.NewTCPServer(shared.TCPPort)
-	go tcpServer.Listen(&handlers.TCPControllerTransportChannelHandler{}) //open tcp server listener with the tcp handler
+	
 	networkInterfaceManager := discovery.NewNetworkInterfaceManager()
 	go discovery.SenderLoop(shared.SOCKET_ID, networkInterfaceManager)
 
